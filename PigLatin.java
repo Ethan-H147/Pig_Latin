28500a7c42e61a0136b2f1f771e6590e879f9af4
Ethan Hu <ethanhu147@gmail.com>
2023-09-08 13:08:36 -0400

    Scan the first letter in the string

--- PigLatin.java
+++ PigLatin.java
@@ -4,11 +4,14 @@ public class PigLatin {
     public static void main(String[] args)
     {
     Scanner scan = new Scanner(System.in);
-    String phrase;
+    String phrase = "";
     phrase = scan.nextLine();
     int length = phrase.length();
-    for(int i=0; i<length;i++){
-    phrase.substring(i, i+1);
-    }
+    String letter;
+    //for(int i=0; i<length;i++){
+    letter = phrase.substring(0, 1);
+    System.out.print(letter);
+   // }
+    
     }
 }
