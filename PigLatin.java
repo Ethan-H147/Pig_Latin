ae110f2b016f5670f44345b56d06dc97cc86feae
Ethan Hu <ethanhu147@gmail.com>
2023-10-23 16:25:36 -0400

    check for first consonant

--- PigLatin.java
+++ PigLatin.java
@@ -5,10 +5,23 @@ public class PigLatin {
     {
     Scanner scan = new Scanner(System.in);
     String word = scan.nextLine();
-    String letter;
-    letter = word.substring(0, 1);
-    String s = word.substring(1);
-    System.out.print(s+letter+"ay");
+    int index=0;
+    char c;
+    do{
+        c = word.charAt(index);
+        index++;
+    }
+    while(!isVowel(c));
+    String firstCon;
+    firstCon = word.substring(0, index-1);
+    String s = word.substring(index-1);
+    System.out.print(s+firstCon+"ay");
     }
 
+public static boolean isVowel(char c){
+    if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
+        return true;
+    }
+    return false;
+}
 }   
